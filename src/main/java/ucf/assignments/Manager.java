1e27e404adeeb6b66583a19d1658743178296d85
Dave <dave031401@gmail.com>
2021-11-01 20:07:42 -0400

    Initial commit

---
+++ src/main/java/ucf/assignments/Manager.java
@@ -0,0 +1,22 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 4 Solution
+ *  Copyright 2021 Dave Edouard
+ */
+
+package ucf.assignments;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import java.io.*;
+
+public class Manager extends Application{
+    public void start(){
+
+    }
+
+}
